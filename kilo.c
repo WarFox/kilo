26b0c36ffffcad3a1c91f402022effb2b66cfbed
Deepu Mohan Puthrote <git@deepumohan.com>
2019-11-22 13:57:15 +0000

    03 - step 42 - Added a welcome message

--- kilo.c
+++ kilo.c
@@ -11,6 +11,8 @@
 
 /*** defines ***/
 
+#define KILO_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
@@ -139,7 +141,26 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+                                "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      abAppend(ab, welcome, welcomelen);
+
+      // center the welcome message
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
+
+    } else {
+      abAppend(ab, "~", 1);
+    }
+
+    abAppend(ab, "\x1b[K", 3); // erase line
 
     // don't write \r\n in the last line
     if (y < E.screenrows - 1) {
@@ -161,7 +182,6 @@ void editorRefreshScreen() {
      <esc>[H will bring the cursor to top left corner
    */
   abAppend(&ab, "\x1b[?25h", 6); // hide cursor with repainting
-  abAppend(&ab, "\x1b[2J", 4);
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
